8071fe4738c9ba929de3fd09d5d881bc04d16378
MohammadAbdorrahmani <mohammad02abdorrahmani@gmail.com>
2024-03-24 18:49:09 +0330

    🎯 fix missing foreign error

--- database/migrations/2024_03_18_155041_create_payments_table.php
+++ database/migrations/2024_03_18_155041_create_payments_table.php
@@ -9,6 +9,8 @@ return new class extends Migration {
     {
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
 
             $table->string('transaction_number');
